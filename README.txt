69dd4660defeafc454de4477d8c504a16db93c3f
Matthew D. Groves <webmaster@mgroves.com>
2014-01-25 16:54:53 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,22 @@
+SQL Profiler Query Cleaner
+
+This is a little project I cooked up in less than an hour to save me some time.
+
+SQL Profiler is a great tool to show you exactly what SQL is being executed, along with the parameters. This is especially useful when debugging, because sometimes you aren't sure what SQL is being executed.
+
+However, often times I will find myself having to pull that SQL+parameters out of SQL Profiler and paste it into SQL Server Studio in order to analyze, tweak, debug, etc. However, SQL Profiler's output looks like:
+
+exec sp_execute sql N'my sql with ''quotes'' escaped string',@MyParam1 datetime, @MyParam2 datetime,@MyParam1='2009-02-11',@MyParam2='2013-03-15'
+
+Which is fine, and *excutes* in SQL Studio, but if there's a syntax error, you can't really pinpoint exactly where in the query the error is, and it's harder to manipulate the parameters because you have to do it in two spots, and it's just time-consuming if you're doing this a lot (which I was).
+
+So, fire up this program, paste the SQL Profiler output into the top text box, click the "Clean" button, and the bottom text box will have a more SQL Studio friendly query, like:
+
+DECLARE @MyParam1 datetime='2009-02-11'
+DECLARE @MyParam2 datetime='2013-03-15'
+
+MY SQL STRING with 'quotes' that AREN'T ESCAPED
+
+To do:
+* This thing uses some really raw string parsing. I would prefer a SQL parser.
+* If the query uses WITH, then I need to add semicolons after the parameters.
---
+++ SqlProfilerQueryCleaner/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SqlProfilerQueryCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputSql = new System.Windows.Forms.TextBox();
+            this.txtOutputSql = new System.Windows.Forms.TextBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInputSql
+            // 
+            this.txtInputSql.Location = new System.Drawing.Point(12, 12);
+            this.txtInputSql.Multiline = true;
+            this.txtInputSql.Name = "txtInputSql";
+            this.txtInputSql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInputSql.Size = new System.Drawing.Size(628, 231);
+            this.txtInputSql.TabIndex = 0;
+            // 
+            // txtOutputSql
+            // 
+            this.txtOutputSql.Location = new System.Drawing.Point(12, 287);
+            this.txtOutputSql.Multiline = true;
+            this.txtOutputSql.Name = "txtOutputSql";
+            this.txtOutputSql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutputSql.Size = new System.Drawing.Size(628, 231);
+            this.txtOutputSql.TabIndex = 1;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(268, 250);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(75, 23);
+            this.btnClean.TabIndex = 2;
+            this.btnClean.Text = "\\/ Clean \\/";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 545);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.txtOutputSql);
+            this.Controls.Add(this.txtInputSql);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputSql;
+        private System.Windows.Forms.TextBox txtOutputSql;
+        private System.Windows.Forms.Button btnClean;
+    }
+}
+
